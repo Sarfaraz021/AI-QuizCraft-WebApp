b48a70e3f585b4c4901a7016cba5873a0c7f8657
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-05-15 03:12:01 +0500

    done with basic rag structure using pinecone

--- App/main.py
+++ App/main.py
@@ -21,7 +21,7 @@ class RAGAssistant:
         self.load_env_variables()
         self.setup_prompt_template()
         self.retriever = None  # Define retriever as an instance variable
-        default_documents_directory = r"D:\DanyAIApp\code\data\dummy.txt"
+        default_documents_directory = r"App\data\dummy.txt"
         self.initialize_retriever(default_documents_directory)
         self.llm = ChatOpenAI(model="gpt-4-turbo-preview", temperature=0.9)
 
@@ -36,24 +36,7 @@ class RAGAssistant:
         """Sets up the prompt template for chat completions."""
         self.template = """
 INSTRUCTIONS:
-Generate Quality Response according to users query, if user says generate long resonse for example upto 1000 words then make sure to generate response upto 1000 words or more.
-You are a helpful assistant that will respond to my queries/prompts in a professional manner. You will answer the question, prompt, or query of the user in the French language if the user's prompt, query, or input is in French, and vice versa for all other languages.
-
-Such as:
-
-Else if the query or user input is in English language, then do response in English.
-For Examples:
-    prompt: what is ai?
-    Asisstant: AI, or Artificial Intelligence, is like teaching a computer or machine to think and learn like a human. It allows machines to perform tasks that usually require human intelligence, such as recognizing speech, making decisions, and solving problems. Just like humans learn from experience, AI improves over time by learning from data.
-
-
-if the query or user input is in French language, then do response in French.
- For Examples:
-    prompt: Qu'est-ce que l'IA ?
-    Asisstant: L'IA, ou Intelligence Artificielle, c'est comme apprendre à un ordinateur ou à une machine à penser et à apprendre comme un humain. Elle permet aux machines d'effectuer des tâches qui nécessitent généralement de l'intelligence humaine, telles que reconnaître la parole, prendre des décisions et résoudre des problèmes. Tout comme les humains apprennent de l'expérience, l'IA s'améliore au fil du temps en apprenant à partir des données.
-
-And follow this for all other langauges.
-
+Generate Quality Response according to users query.
 <ctx>
 {context}
 </ctx>
@@ -137,24 +120,3 @@ Answer:
                 assistant_response = chain.invoke(prompt)  # type: ignore
                 print(f"AI Assistant: {assistant_response['result']}")
                 print("*********************************")
-
-    def start(self):
-        """Main function to start the assistant."""
-        while True:
-            choice = input(
-                "\nEnter 1 for RAG Assistant chat or 2 to Fine-tune RAG: ").strip()
-            if choice == "1":
-                self.chat()
-            elif choice == "2":
-                path = input(
-                    "Enter directory path to fine-tune the RAG: ").strip()
-                self.finetune(path)
-                print(
-                    "\nFine-tuning done successfully. You can now chat with the updated RAG Assistant.\n")
-            else:
-                print("Invalid choice. Please enter 1 or 2.")
-
-
-if __name__ == "__main__":
-    rag_assistant = RAGAssistant()
-    rag_assistant.start()
